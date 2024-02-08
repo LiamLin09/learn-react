b7d08e064f46f2e6a5db448df7a1a453d7982e92
LiamLin <137009133+LiamLin09@users.noreply.github.com>
2024-02-07 23:00:54 -0500

    Update todos.js

--- src/pages/qcomps/todos.js
+++ src/pages/qcomps/todos.js
@@ -1,22 +1,29 @@
-const baseUrl = 'https://i.imgur.com/';
-const person = {
-  name: 'Gregorio Y. Zara',
-  imageId: '7vQD0fP',
-  imageSize: 's',
-  theme: {
-    backgroundColor: 'black',
-    color: 'pink'
+function Item({ name, isPacked }) {
+  if (isPacked) {
+    return <li className="item">
+      <del>{name}</del> </li>;
   }
-};
+  return <li className="item"> {name} </li>
+}
 
-export default function TodoList() {
+export default function PackingList2() {
   return (
-    <div style={person.theme}>
-      <h1>Person Name's Todos</h1>
-      <img className="avatar"
-      src={`${baseUrl}${person.imageId}${person.imageSize}.jpg`}
-      alt={person.name}
-      />
-    </div>
+    <section>
+      <h1>My Packing List</h1>
+      <ul>
+        <Item
+          isPacked={false}
+          name="Laptop"
+        />
+        <Item
+          isPacked={true}
+          name="Chargers"
+        />
+        <Item
+          isPacked={true}
+          name="Socks"
+        />
+      </ul>
+    </section>
   );
 }
